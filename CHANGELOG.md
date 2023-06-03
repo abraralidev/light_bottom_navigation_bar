5a40ab60cfe8dfe412ffbb9b5660562459a7cef3
abraralidev <abraralidev@gmail.com>
2023-06-03 13:08:32 +0500

    version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
-## [0.0.1]
+## [0.0.2]
 
 * This widget help you to make pro apps with high advanced UI.
+* Example code added.
---
+++ example/android/app/src/main/kotlin/com/example/examplecode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.examplecode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
